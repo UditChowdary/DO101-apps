33823bb683e68f96147204d47f9169e9f10537b4
UditChowdary <uditchowdary1298@gmail.com>
2024-10-24 13:10:46 -0400

    Weather update

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res) {
 
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${Washington,DC}&units=${Farenheit}&appid=${4241552e89d60cefaab252aa61f465f0}`;
 
   try {
     let data = await fetch(url);
